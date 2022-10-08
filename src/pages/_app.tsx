3b17a4985c4b06f92d9f19abfe7e638573c1106f
ktakaham <per.tkhm@gmail.com>
2022-10-08 22:22:55 +0900

    [fix]emotion cache設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,26 @@ import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
 import { ApolloContextProvider } from "@/utils/apollo/apolloClient";
+import type { EmotionCache } from "@emotion/react";
+import { CacheProvider } from "@emotion/react";
+import { createEmotionCache } from "@/utils/mui/createEmotionCache";
 
-const App = ({ Component, pageProps }: AppProps) => {
+const clientSideEmotionCache = createEmotionCache();
+
+interface AppPropsExtend extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const App = ({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: AppPropsExtend) => {
   return (
     <ApolloContextProvider pageProps={pageProps}>
-      <Component {...pageProps} />
+      <CacheProvider value={emotionCache}>
+        <Component {...pageProps} />
+      </CacheProvider>
     </ApolloContextProvider>
   );
 };
